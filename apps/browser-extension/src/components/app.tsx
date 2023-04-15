0dbb26252262272c16a839ac776d657bb4c4979a
RuariM <48099261+RuariM@users.noreply.github.com>
2023-04-15 03:00:56 +0100

    progress bar

--- apps/browser-extension/src/components/app.tsx
+++ apps/browser-extension/src/components/app.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 import { USER_COOKIE_NAME } from "~globals"
 import useRefresh from "~src/hooks/mutations/useRefresh"
@@ -9,46 +9,49 @@ import { getCookie } from "~src/utils/getCookie"
 import Button from "./button"
 import { Progress } from "./progress"
 
-type Schedule = 6 | 12 | 24 | undefined
-
 const App = () => {
   const [isRefreshing, setRefreshing] = useState(false)
   const [isOpen, setOpen] = useState(false)
-  const [selected, setSelected] = useState<Schedule>()
+  const [selected, setSelected] = useState<number>()
   const [refreshProgress, setRefreshProgress] = useState(0)
+  const [numProducts, setNumProducts] = useState(0)
 
   const user_id = getCookie(USER_COOKIE_NAME)
   const { mutateAsync: refresh } = useRefresh()
 
+  //get current Schedule
   //will come from api
   //getUser()
   //user.schedule
 
-  const isSelected = (selected: Schedule, schedule: Schedule) => {
+  const isSelected = (selected: number, schedule: number) => {
     return selected == schedule ? true : false
   }
-  //get currentSchedual
 
   const handleRefresh = async () => {
-    setRefreshing(true)
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms))
 
+    setRefreshing(true)
     try {
       const products = (await useGetShopProducts(user_id)).flat()
-
-      products.map(async (product: Product) => {
+      setNumProducts(products.length)
+      for (const product of products) {
         try {
+          setRefreshProgress((prevState) => prevState + 1)
           const productData = await useGetProduct(product.slug)
           await refresh({ slug: product.slug, product: productData.data })
+          await delay(500)
         } catch (e) {
           console.log(e)
         }
-      })
+      }
     } catch (e) {
       console.log(e)
     }
     setRefreshing(false)
+    setRefreshProgress(0)
   }
-  const handleSchedule = (schedule: Schedule) => {
+  const handleSchedule = (schedule: number) => {
     //this should post to schedule endpoint for current user_id
     //replace setSeleced with mutateSchedule calling api
     if (isSelected(selected, schedule)) setSelected(undefined)
@@ -57,14 +60,19 @@ const App = () => {
 
   if (!isOpen)
     return (
-      <div className="fixed bottom-0 right-0 z-50 p-6 font-sans">
+      <button className="fixed bottom-0 right-0 z-50 p-6 font-sans">
         <div
           className="w-38 h-38 rounded-md p-5 text-white bg-black"
           onClick={() => setOpen(true)}>
           <h1 className="font-bold text-2xl">[A-H]</h1>
         </div>
-      </div>
+      </button>
     )
+  const scheduleOptions = [
+    { content: "6hrs", interval: 6 },
+    { content: "12hrs", interval: 12 },
+    { content: "24hrs", interval: 24 },
+  ]
 
   //animate transition on open with headless or radix
   if (isOpen)
@@ -72,26 +80,24 @@ const App = () => {
       <div style={{ all: "initial" }}>
         <div className="fixed bottom-0 right-0 z-50 p-6 font-sans">
           <div className="shadow-lg rounded-md p-5 space-y-2 bg-black text-white">
-            {isRefreshing && <ProgressBar />}
+            {isRefreshing && (
+              <ProgressBar
+                currentProgress={refreshProgress}
+                max={numProducts}
+              />
+            )}
             {/* use map and array */}
             <Button onClick={handleRefresh} content="Refresh All" />
             <div className="space-y-2">
               <h2 className="text-center w-full ">Schedule</h2>
-              <Button
-                isSelected={isSelected(selected, 6)}
-                onClick={() => handleSchedule(6)}
-                content="6hrs"
-              />
-              <Button
-                isSelected={isSelected(selected, 12)}
-                onClick={() => handleSchedule(12)}
-                content="12Hrs"
-              />
-              <Button
-                isSelected={isSelected(selected, 24)}
-                onClick={() => handleSchedule(24)}
-                content="24hrs"
-              />
+              {scheduleOptions.map((option, i) => (
+                <Button
+                  key={i}
+                  isSelected={isSelected(selected, option.interval)}
+                  onClick={() => handleSchedule(option.interval)}
+                  content={option.content}
+                />
+              ))}
             </div>
             <button
               className="hover:underline w-full text-center"
@@ -104,14 +110,14 @@ const App = () => {
     )
 }
 
-const ProgressBar = () => {
-  //should take from prop
-  const [progress, setProgress] = useState(13)
-
-  useEffect(() => {
-    const timer = setTimeout(() => setProgress(66), 500)
-    return () => clearTimeout(timer)
-  }, [progress])
+const ProgressBar = ({
+  currentProgress,
+  max,
+}: {
+  currentProgress: number
+  max: number
+}) => {
+  const progress = Math.floor((currentProgress / max) * 100)
 
   return (
     <div className="text-white space-y-1">
