678260e0bfacc356988b66b18d09b17358e009a5
RuariM <48099261+RuariM@users.noreply.github.com>
2023-05-24 21:51:19 +0100

    clean up

--- apps/browser-extension/src/popup.tsx
+++ apps/browser-extension/src/popup.tsx
@@ -1,9 +1,4 @@
 function IndexPopup() {
-  const handleDisable = (onOff: 0 | 1) => {
-    //set localStorage
-    //inject function that hides/shows widget
-  }
-
   return <div style={{ all: "initial" }}>AUTO HUSTLER</div>
 }
 
