f6aa661f37ddb2df420e1bd3b8b34343a358d99c
RuariM <48099261+RuariM@users.noreply.github.com>
2023-05-24 21:48:57 +0100

    clean up pop up

--- apps/browser-extension/src/popup.tsx
+++ apps/browser-extension/src/popup.tsx
@@ -4,11 +4,7 @@ function IndexPopup() {
     //inject function that hides/shows widget
   }
 
-  return (
-    <div style={{ all: "initial" }}>
-      AUTO HUSTLER <div className="text-red-400">On / Off</div>
-    </div>
-  )
+  return <div style={{ all: "initial" }}>AUTO HUSTLER</div>
 }
 
 export default IndexPopup
