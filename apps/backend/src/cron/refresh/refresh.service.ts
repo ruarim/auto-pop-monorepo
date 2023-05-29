5192e602cb70a073f8a8ecdcd322d42499fdcb3c
RuariM <48099261+RuariM@users.noreply.github.com>
2023-05-29 21:10:01 +0100

    change cron function names

--- apps/backend/src/cron/refresh/refresh.service.ts
+++ apps/backend/src/cron/refresh/refresh.service.ts
@@ -9,17 +9,17 @@ export class RefreshCronService {
   constructor(private readonly usersService: UsersService) {}
 
   @Cron(CronExpression.EVERY_6_HOURS)
-  async handle3HourRefreshCron() {
+  async handle6HourRefreshCron() {
     await this.refreshAllProducts(6);
   }
 
   @Cron(CronExpression.EVERY_12_HOURS)
-  async handle6HourRefreshCron() {
+  async handle12HourRefreshCron() {
     await this.refreshAllProducts(12);
   }
 
   @Cron(CronExpression.EVERY_DAY_AT_9AM)
-  async handle12HourRefreshCron() {
+  async handle24HourRefreshCron() {
     await this.refreshAllProducts(24);
   }
 
