3276396eaaf83980a05acb7fcc1a93fc86387bfc
RuariM <48099261+RuariM@users.noreply.github.com>
2023-05-30 14:38:07 +0100

    dot env

--- apps/browser-extension/globals.ts
+++ apps/browser-extension/globals.ts
@@ -1,8 +1,7 @@
-export const DEV_ENV = true //set to false in prod
 export const DOMAIN = ".depop.com"
 export const ACCESS_COOKIE_NAME = "access_token="
 export const USER_COOKIE_NAME = "user_id"
 export const DEPOP_API_ENDPOINT = "https://webapi.depop.com/api"
-export const BACKEND_API_ENDPOINT = DEV_ENV
+export const BACKEND_API_ENDPOINT = process.env.DEV_ENV
   ? "http://localhost:3000"
-  : "DEPLOYED ENDPOINT"
+  : process.env.BACKEND_URL
