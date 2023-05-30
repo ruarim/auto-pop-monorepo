1325f6e7fe06d7b5081076a150e1aedb97029422
RuariM <48099261+RuariM@users.noreply.github.com>
2023-05-30 19:05:59 +0100

    revert

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
       origin: 'https://www.depop.com',
       allowedHeaders: [
         'Origin',
+        'X-Requested-With',
         'Content-Type',
         'Accept',
         'Authorization',
