29d2f3c3220d46acc02c937eafd94d8573ea17a8
RuariM <48099261+RuariM@users.noreply.github.com>
2023-06-07 17:01:13 +0100

    add my mysql

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,21 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { RefreshCronService } from './cron/refresh/refresh.service';
+import { getTypeOrmConfig } from 'config/typeorm.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) =>
+        await getTypeOrmConfig(configService),
+      inject: [ConfigService],
     }),
     ScheduleModule.forRoot(),
     UsersModule,
