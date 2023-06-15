0ed86cb4ed2aa9d276916210f414a40ef3e8f91e
RuariM <48099261+RuariM@users.noreply.github.com>
2023-06-15 17:36:54 +0100

    use node env

--- apps/browser-extension/globals.ts
+++ apps/browser-extension/globals.ts
@@ -3,6 +3,6 @@ export const ACCESS_COOKIE_NAME = "access_token="
 export const USER_COOKIE_NAME = "user_id"
 export const DEPOP_API_ENDPOINT = "https://webapi.depop.com/api"
 export const BACKEND_API_ENDPOINT =
-  process.env.DEV_ENV === "true"
+  process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
     : process.env.BACKEND_URL
